3ffaa3abc859a8bf4ff413fef62f1ef8efc40ce6
shivani6209 <55498909+shivani6209@users.noreply.github.com>
2019-09-18 21:53:08 +0530

    Add files via upload

---
+++ shivani.ino
@@ -0,0 +1,15 @@
+void setup() 
+{
+  int ledPin=13;
+  pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() 
+{
+  digitalWrite(ledPin 13,HIGH);
+  delay(1000);
+  digitalWrite(ledPin 13,LOW);
+  delay(1000);
+
+}
